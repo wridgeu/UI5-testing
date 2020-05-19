e5101360c99017712eef44ef0a0c227b9c08dcad
Marco Beier <marco-beier@t-online.de>
2020-05-19 21:19:42 +0200

    added rootView configuration

---
+++ webapp/controller/App.Controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"com/mrb/UI5-Testing/controller/BaseController"
+], function(
+	BaseController
+) {
+	"use strict";
+
+	return BaseController.extend("com.mrb.UI5-Testing.controller.App.Controller", {
+	});
+});
