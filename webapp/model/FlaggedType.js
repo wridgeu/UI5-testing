61ce480f3ee24473f6deaf54e95e1b833ab638af
Marco Beier <marco-beier@t-online.de>
2020-05-23 15:09:35 +0200

    added info about implementation in code comments

--- webapp/model/FlaggedType.js
+++ webapp/model/FlaggedType.js
@@ -3,10 +3,13 @@ sap.ui.define([
 ], function (SimpleType) {
 	"use strict";
 	return SimpleType.extend("com.mrb.UI5-Testing.model.FlaggedType", {
+		//formats a model value to be displayed in the UI
 		formatValue: function () {
 		},
+		//parses a UI value to be stored in the model
 		parseValue: function () {
 		},
+		//checks a value for displaying validation errors
 		validateValue: function () {
 		}
 	});
