d248134559f072ee6c83bc0b244b64fa530e5838
Marco Beier <marco-beier@t-online.de>
2020-05-23 15:38:35 +0200

    added a slight hint to simpletype

--- webapp/model/FlaggedType.js
+++ webapp/model/FlaggedType.js
@@ -2,6 +2,7 @@ sap.ui.define([
 	"sap/ui/model/SimpleType"
 ], function (SimpleType) {
 	"use strict";
+	//Use data types if you need both formatting and parsing of a model value (two way instead of formatting - one way)
 	return SimpleType.extend("com.mrb.UI5-Testing.model.FlaggedType", {
 		//formats a model value to be displayed in the UI
 		formatValue: function () {
