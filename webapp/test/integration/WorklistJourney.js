27b0b20e14b5a0c79afd777e1cc85d4348d28eb4
Marco Beier <marco-beier@t-online.de>
2020-05-25 20:56:20 +0200

    Added everything for Step 6: A First OPA Test

--- webapp/test/integration/WorklistJourney.js
+++ webapp/test/integration/WorklistJourney.js
@@ -1,22 +1,26 @@
 /*global QUnit*/
-sap.ui.define([
-	"sap/ui/test/opaQunit",
-	"./pages/Worklist"
-],  function (opaTest) {
-	"use strict";
+sap.ui.define(["sap/ui/test/opaQunit", "./pages/Worklist"], function (opaTest) {
+  "use strict";
 
-	QUnit.module("Posts");
+  QUnit.module("Posts");
 
-	opaTest("Should see the table with all posts", function (Given, When, Then) {
-		// Arrangements
-		Given.iStartMyApp();
+  opaTest("Should see the table with all posts", function (Given, When, Then) {
+    // Arrangements
+    Given.iStartMyApp();
 
-		// Assertions
-		Then.onTheWorklistPage.theTableShouldHaveAllEntries().
-			and.theTitleShouldDisplayTheTotalAmountOfItems();
+    // Assertions
+    Then.onTheWorklistPage
+      .theTableShouldHavePagination()
+      .and.theTitleShouldDisplayTheTotalAmountOfItems();
+  });
 
-		// Cleanup
-		Then.iTeardownMyApp();
-	});
+  opaTest("Should be able to load more items", function (Given, When, Then) {
+    //Actions
+    When.onTheWorklistPage.iPressOnMoreData();
 
+    // Assertions
+    Then.onTheWorklistPage.theTableShouldHaveAllEntries();
+    // Cleanup
+    Then.iTeardownMyApp();
+  });
 });
